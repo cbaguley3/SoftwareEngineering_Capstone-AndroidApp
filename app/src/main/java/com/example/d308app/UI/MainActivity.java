12e8df6b5832a3883ad25108dcc7a8240ab18dfa
Charles <charlesbaguley@gmail.com>
2024-05-17 13:09:36 -0500

    pre final commit for step A

---
+++ app/src/main/java/com/example/d308app/UI/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.d308app.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.d308app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button button =findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,VacationList.class);
+            intent.putExtra("test", "information sent");
+            startActivity(intent);
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

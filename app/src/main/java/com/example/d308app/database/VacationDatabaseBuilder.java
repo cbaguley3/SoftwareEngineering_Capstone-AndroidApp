7e17431b134fb87d69298e77ff6ac9fbf482b5ae
Charles <charlesbaguley@gmail.com>
2024-06-09 11:55:37 -0400

    Final commit for submission (added readme)

--- app/src/main/java/com/example/d308app/database/VacationDatabaseBuilder.java
+++ app/src/main/java/com/example/d308app/database/VacationDatabaseBuilder.java
@@ -11,7 +11,7 @@ import com.example.d308app.dao.VacationDAO;
 import com.example.d308app.entities.Excursion;
 import com.example.d308app.entities.Vacation;
 
-@Database(entities = {Vacation.class, Excursion.class}, version= 8, exportSchema = false)
+@Database(entities = {Vacation.class, Excursion.class}, version= 9, exportSchema = false)
 public abstract class VacationDatabaseBuilder extends RoomDatabase {
     public abstract VacationDAO vacationDAO();
     public abstract ExcursionDAO excursionDAO();

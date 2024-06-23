3be877e4ee041f934865d39bd6e1041bf2e7e572
Charles <charlesbaguley@gmail.com>
2024-06-23 16:01:32 -0400

    Second commit for Capstone Project. Added search bar feature to existing application.

--- app/src/main/java/com/example/d308app/UI/VacationAdaptor.java
+++ app/src/main/java/com/example/d308app/UI/VacationAdaptor.java
@@ -1,84 +1,73 @@
 package com.example.d308app.UI;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.d308app.R;
 import com.example.d308app.entities.Vacation;
-
+import java.util.ArrayList;
 import java.util.List;
 
-
 public class VacationAdaptor extends RecyclerView.Adapter<VacationAdaptor.VacationViewHolder> {
-
-    private List<Vacation> mVacations;
-    private final Context context;
-
-    private final LayoutInflater mInflator;
+    private final LayoutInflater mInflater;
+    private List<Vacation> mVacations; // Cached copy of vacations
+    private List<Vacation> mVacationsFiltered; // List for filtered vacations
 
     public VacationAdaptor(Context context) {
-        mInflator = LayoutInflater.from(context);
-        this.context = context;
+        mInflater = LayoutInflater.from(context);
+        mVacations = new ArrayList<>();
+        mVacationsFiltered = new ArrayList<>();
     }
 
-    public class VacationViewHolder extends RecyclerView.ViewHolder{
-        private final TextView vacationItemView;
-
-        public VacationViewHolder(@NonNull View itemView) {
-            super(itemView);
-            vacationItemView=itemView.findViewById(R.id.textView2);
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int position = getAdapterPosition();
-                    final Vacation current = mVacations.get(position);
-                    Intent intent = new Intent(context, VacationDetails.class);
-                    intent.putExtra("id", current.getVacationID());
-                    intent.putExtra("name", current.getVacationName());
-                    intent.putExtra("lodging", current.getLodging());
-                    intent.putExtra("startDate", current.getStartDate());
-                    intent.putExtra("endDate", current.getEndDate());
-                    context.startActivity(intent);
-                }
-            });
-        }
-    }
-
-    @NonNull
     @Override
-    public VacationAdaptor.VacationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView=mInflator.inflate(R.layout.vacation_list_item,parent, false);
+    public VacationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.recyclerview_item, parent, false);
         return new VacationViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull VacationAdaptor.VacationViewHolder holder, int position) {
-        if(mVacations != null) {
-            Vacation current = mVacations.get(position);
-            String name = current.getVacationName();
-            holder.vacationItemView.setText(name);
+    public void onBindViewHolder(VacationViewHolder holder, int position) {
+        if (mVacationsFiltered != null && !mVacationsFiltered.isEmpty()) {
+            Vacation current = mVacationsFiltered.get(position);
+            holder.vacationItemView.setText(current.getVacationName());
         }
-        else holder.vacationItemView.setText("No vacation name");
+    }
+
+    void setVacations(List<Vacation> vacations) {
+        mVacations = vacations;
+        mVacationsFiltered = new ArrayList<>(vacations);
+        notifyDataSetChanged();
     }
 
     @Override
     public int getItemCount() {
-        if (mVacations != null) {
-            return mVacations.size();
-        }
-        else return 0;
+        return mVacationsFiltered != null ? mVacationsFiltered.size() : 0;
     }
 
-    public void setVacations(List<Vacation> vacations) {
-        mVacations = vacations;
+    public void filter(String text) {
+        mVacationsFiltered.clear();
+        if (text.isEmpty()) {
+            mVacationsFiltered.addAll(mVacations);
+        } else {
+            text = text.toLowerCase();
+            for (Vacation vacation : mVacations) {
+                if (vacation.getVacationName().toLowerCase().contains(text)) {
+                    mVacationsFiltered.add(vacation);
+                }
+            }
+        }
         notifyDataSetChanged();
     }
 
+    class VacationViewHolder extends RecyclerView.ViewHolder {
+        private final TextView vacationItemView;
+
+        private VacationViewHolder(View itemView) {
+            super(itemView);
+            vacationItemView = itemView.findViewById(R.id.textView);
+        }
+    }
 }
--- app/src/main/java/com/example/d308app/UI/VacationList.java
+++ app/src/main/java/com/example/d308app/UI/VacationList.java
@@ -5,26 +5,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.d308app.R;
 import com.example.d308app.database.Repository;
-import com.example.d308app.entities.Excursion;
 import com.example.d308app.entities.Vacation;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import java.util.List;
 
 public class VacationList extends AppCompatActivity {
     private Repository repository;
+    private VacationAdaptor vacationAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,33 +27,43 @@ public class VacationList extends AppCompatActivity {
         setContentView(R.layout.activity_vacation_list);
         FloatingActionButton fab = findViewById(R.id.floatingActionButton);
 
-        fab.setOnClickListener((new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(VacationList.this, VacationDetails.class);
                 startActivity(intent);
             }
-        }));
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        });
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         repository = new Repository(getApplication());
         List<Vacation> allVacations = repository.getmAllVacations();
-        final VacationAdaptor vacationAdaptor = new VacationAdaptor(this);
+        vacationAdaptor = new VacationAdaptor(this);
         recyclerView.setAdapter(vacationAdaptor);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         vacationAdaptor.setVacations(allVacations);
-        // System.out.println(getIntent().getStringExtra("test"));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_vacation_list, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Search for a vacation");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (vacationAdaptor != null) {
+                    vacationAdaptor.filter(newText);
+                }
+                return false;
+            }
+        });
         return true;
     }
 
@@ -67,7 +71,6 @@ public class VacationList extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             Intent intent = new Intent(this, MainActivity.class);
-            //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK); // Clear the back stack and start a new task
             startActivity(intent);
             finish(); // Close the current activity
             return true;
